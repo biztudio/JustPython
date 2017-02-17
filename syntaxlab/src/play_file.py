e8f8f8363ac5623e616f7e5a5acb5951ed57a922
Yang SHEN <bizbetter@outlook.com>
2017-02-18 00:12:39 +0800

    add comment
    顺便区分一下字符串 .index 和 .find 的区别
    对于找不到的情况， .index 直接抛substring not found 的异常，而用 .find 则返回 -1，在能找到的情况下效果则类似

--- syntaxlab/src/play_file.py
+++ syntaxlab/src/play_file.py
@@ -12,6 +12,7 @@ class AssemblyReader:
             with open(self.file, 'r', encoding='utf8') as file:  # 加上encoding='utf8'才能确保读取文件时正确处理版本字符 ©
                 # for line in file.readlines():
                 for line in file:  # 相较于readlines, 迭代占用更小的内存，而且更加智能（依赖于Python文件对象的实现），所需文件内容是自动从buffer中按照需要读取
+                    # 此处不用.index 是因为有的行没有包含 [assembly:AssemblyVersion( 时，程序直接抛substring not found 的异常，而用 .find 则返回 -1，在能找到的情况下效果则类似
                     find_flag = line.strip().replace(' ', '').find('[assembly:AssemblyVersion(')
                     if find_flag >= 0 > line.strip().find('//'):  # 过滤单行注释文本, 本行布尔判断的写法很好玩
                         self.assembly_version = line.split('"')[1]
